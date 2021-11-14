00d805b354caaa3718912cc55ef7ea7a38c14ca6
WelderBM <Welderb40@gmail.com>
2021-11-14 17:19:49 -0300

    New finished project

---
+++ script.js
@@ -0,0 +1,44 @@
+const button = document.getElementById("convertButton")
+const select = document.getElementById("currencySelect")
+const currencyImg = document.getElementById("currencyImg")
+
+const dolar = 5.2
+const euro = 5.9
+
+const currencyDolar = (money) => {
+    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(money)
+}
+const currencyReal = (money) => {
+    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 }).format(money)
+}
+const currencyEuro = (money) => {
+    return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 2 }).format(money)
+}
+
+const convertValues = () => {
+    const realValue = document.getElementById("idRealValue").value
+    document.getElementById("idRealValueText").innerHTML = `${currencyReal(realValue)}`
+    if (select.value === "€ Euro") {
+        const newEuro = realValue / euro
+        document.getElementById("currencyValueText").innerHTML = `${currencyEuro(newEuro)}`
+    } else {
+        const newDolar = realValue / dolar
+        document.getElementById("currencyValueText").innerHTML = `${currencyDolar(newDolar)}`
+    }
+
+}
+
+changeCurrency = () => {
+    const currencyName = document.getElementById("idCurrencyName")
+    if (select.value === "€ Euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./images/Euro.png"
+    } else {
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImg.src = "./images/estados-unidos (1) 1.png"
+    }
+    convertValues()
+}
+
+button.addEventListener("click", convertValues)
+select.addEventListener("change", changeCurrency)

6ef71a9c075bb3b057a33f2624bb69cbdff38a3d
WelderBM <Welderb40@gmail.com>
2021-11-15 16:11:56 -0300

    finished the project

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const button = document.getElementById("convertButton")
 const select = document.getElementById("currencySelect")
 const currencyImg = document.getElementById("currencyImg")
 
-const dolar = 5.2
-const euro = 5.9
+const dolar = 5.42
+const euro = 6.25
+const bitCoin = 350120.33
 
 const currencyDolar = (money) => {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(money)
@@ -21,21 +22,32 @@ const convertValues = () => {
     if (select.value === "€ Euro") {
         const newEuro = realValue / euro
         document.getElementById("currencyValueText").innerHTML = `${currencyEuro(newEuro)}`
-    } else {
+    } 
+    if (select.value === "US$ Dolar Americano"){
         const newDolar = realValue / dolar
         document.getElementById("currencyValueText").innerHTML = `${currencyDolar(newDolar)}`
+    }
+    if (select.value === "₿ BitCoin") {
+        const newBitCoin = realValue / bitCoin 
+        document.getElementById("currencyValueText").innerHTML = `${newBitCoin}`
     }
 
 }
 
 changeCurrency = () => {
     const currencyName = document.getElementById("idCurrencyName")
+
     if (select.value === "€ Euro") {
         currencyName.innerHTML = "Euro"
         currencyImg.src = "./images/Euro.png"
-    } else {
+    } 
+    if (select.value === "US$ Dolar Americano"){
         currencyName.innerHTML = "Dólar Americano"
         currencyImg.src = "./images/estados-unidos (1) 1.png"
+    }
+    if (select.value === "₿ BitCoin") {
+        currencyName.innerHTML = "BitCoin"
+        currencyImg.src = "./images/BitCoin.png"
     }
     convertValues()
 }
